2106f80882d7cfe44d7bd7fb5ca2cb57e0bc22ab
Enzo <enzo@mallard.ml>
2021-04-09 14:28:19 +0200

    Add db committer

---
+++ gmsab.py
@@ -0,0 +1,22 @@
+# Export the generated art to an S3 bucket and a db server
+
+
+def save_s3(art_data):
+    # Use s3cmd to send the file (sensitive data are handled externally)
+    import subprocess
+    s3cmd = subprocess.run(['s3cmd', 'put', art_data['filename'], 's3://gmsab/', '--content-type', 'image/jpeg'])
+    return s3cmd.returncode
+
+
+def save_db(art_data):
+    import datetime
+    import requests
+    import json
+    url = open('store.url').readline()  # API url is provided via a local token
+    payload = dict()
+    payload['generated'] = int(datetime.datetime.now().timestamp() * 1000)  # create JS date
+    payload['seed'] = art_data['seed']
+    payload['size'] = int(art_data['size'])
+    payload['url'] = art_data['filename']
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from art import Art
+from gmsab import save_s3, save_db
 import random
 
 if __name__ == '__main__':
     random.seed(None)
     generated = Art(size=2**8)
     print("Making art with the seed %s" % repr(generated.x))
-    artData = generated.redraw()
-    print("saved to %s (Size %s)\nSEED: %s" % (artData['filename'], artData['size'], repr(artData['seed'])))
+    art_data = generated.redraw()
+    print("saved to %s (Size %s)\nSEED: %s" % (art_data['filename'], art_data['size'], repr(art_data['seed'])))
+
+    # save the generated image for the online viewer
+    if save_s3(art_data) == 0:
+        save_db(art_data)
